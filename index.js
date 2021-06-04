039ad52a2b3f547c5f513f18f8edb75e2d8c64c1
JY <jypeace@yahoo.com>
2021-06-04 13:10:49 +0800

    play.js

---
+++ index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer((req,res)=>{
+  fs.readFile('index.html',(err, data)=>{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  })
+//    res.write("Hello World from index.js, jyydev.github.io");
+//    res.end();
+
+}).listen(process.env.PORT);
